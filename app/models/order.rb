be3915c8e61bd3a8ad234065b61ffbdd3d4b2405
ishimaru-libertyfish <39154900+ishimaru-libertyfish@users.noreply.github.com>
2026-04-10 08:46:52 +0000

    OCA20260410

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
   has_many :order_details
   has_many :products, through: :order_details
 
-  validates :count, numericality: { only_integer: true, greater_than: 0 }
+  #validates :count, numericality: { only_integer: true, greater_than: 0 }
   validates :address, presence: true
 
 end
